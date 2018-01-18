ecd6bc72179e172a98e43fd585f84e7303226933
ТогузбаевА <abai2010@bk.ru>
2018-01-18 11:59:51 +0600

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Order> orders;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            orders = new ObservableCollection<Order>()
+            {
+                new Order
+                {
+                    Id=1,
+                    Name="Булочка",
+                    Price=50,
+                    ExpirationDate = DateTime.Now.AddDays(1)
+                },
+                new Order
+                {
+                    Id=2,
+                    Name="Хлеб",
+                    Price=70,
+                    ExpirationDate = DateTime.Now.AddDays(1)
+                }
+            };
+
+            dataContainer.ItemsSource = orders;
+        }
+
+        private void dataContainerAddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            orders.Add(new Order
+            {
+                Id = 2,
+                Name = "Сникерс",
+                Price = 70,
+                ExpirationDate = DateTime.Now.AddDays(1)
+            });
+        }
+    }
+}
---
+++ WpfApp6/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    public class Order:INotifyPropertyChanged
+    {
+        private int id;
+        private string name;
+        private double price;
+        private DateTime expirationDate;
+        public int Id
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+                //то же с остальными
+            }
+        }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+            
+        }
+    }
+}
